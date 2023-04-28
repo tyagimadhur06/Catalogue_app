879cef584ffd83fc1d46e2c8ce66a7abb2c43277
tyagimadhur06 <tyagimadhur06@gmail.com>
2023-04-28 23:36:26 +0530

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/home_page.dart';
+import 'package:myapp/pages/home_page.dart';
+import 'package:myapp/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/utils/routes.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -11,7 +14,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => LoginPage(),
+        MyRoutes.homeRoute :(context) => HomePage(),
+        MyRoutes.loginRoute:(context) => LoginPage(),
+      },
     );
     return const Placeholder();
   }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,6 @@
+import 'package:myapp/pages/home_page.dart';
+
+class MyRoutes {
+  static String loginRoute = "/login";
+  static String homeRoute = "/home";
+}
